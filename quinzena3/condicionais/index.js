6c9d64cc3376741b4660fb22c5b7730f85e67ec1
Leticia Felix <leticiafs@hotmail.com.br>
2020-09-22 17:31:27 -0300

    Exercício de Condicionais

---
+++ quinzena3/condicionais/index.js
@@ -0,0 +1,65 @@
+//console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+//EXERCÍCIO 1
+
+// O código analisa se o resto da divisão do número indicado pelo usuário é 0, concluindo se o número é par ou impar.
+// Se o número for par, a divisão não vai ter resto e vai ser impresso no console o "Passou no teste".
+// Se o número for impar, a divisão vai ter resto e vai ser impresso no console o "Não passou no teste"
+
+// EXERCÍCIO 2
+
+// A. Para mostrar o preço das frutas listadas.
+// B. "O preço da fruta Maçã é R$ 2,25".
+// C. "O preço da fruta Pêra é R$ 5". 
+
+// EXERCÍCIO 3
+
+// A. Checando se o número digitado pelo usuário é maior que 0.
+// B. Para o número 10 seria: "Esse número passou no teste". Para -10, não teria mensagem impressa.
+// C. Sim, pois nenhuma mensagem foi atribuida para números menores de 0.
+
+// EXERCÍCIO 4
+
+// const idade = Number(prompt("Qual a sua idade?"))
+
+// if (idade >= 18) {
+//     console.log("Você pode dirigir.")
+// } else {
+//     console.log("Você não pode dirigir.")
+// }
+
+// EXERCÍCIO 5
+
+// const turno = prompt("Qual seu turno de estudo? Responda M (matutino), V (Vespertino) ou N (Noturno).")
+
+// if (turno === "M") {
+//     console.log("Bom dia!")
+// } else if (turno === "V") {
+//     console.log("Boa Tarde!")
+// } else if (turno === "N")
+//     console.log("Boa Noite!");
+
+// EXERCÍCIO 6
+
+// const turno = prompt("Qual seu turno de estudo? Responda M (matutino), V (Vespertino) ou N (Noturno).")
+//     switch(turno){
+//     case "M":
+//     console.log("Bom dia!")
+//     break
+//     case "V":
+//     console.log("Boa Tarde!")
+//     break
+//     case "N":
+//     console.log("Boa Noite!")
+//     break
+//     }
+
+// EXERCÍCIO 7
+
+// const filme = prompt("Qual o gênero de filme que vão assistir?")
+// const ingresso = Number(prompt("Quanto custa o ingresso?"))
+
+//     if (filme === "fantasia", ingresso < "15"){
+//         console.log("Bom filme!");
+// } else  (ingresso < "15")
+//     console.log("Escolha outro filme! :(");
