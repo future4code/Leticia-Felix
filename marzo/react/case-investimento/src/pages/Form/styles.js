01639283f088a3e2df7909904671ea9079e60e03
Leticia Felix <leticiafs@hotmail.com.br>
2022-02-17 17:23:22 -0300

    Início de construção da lógica do form e CSS

--- marzo/react/case-investimento/src/pages/Form/styles.js
+++ marzo/react/case-investimento/src/pages/Form/styles.js
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
 export const FormContainer = styled.div`
+width: 25rem;
+height: 20rem;
+display: flex;
+flex-direction:column;
+justify-content: space-between;
+align-items: center;
+margin-top: 8rem;
+background-color: aquamarine;
+border-radius: 50px;
+box-shadow: 0px 4px 6px #00000029;
+`
 
+export const Formulario = styled.form`
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1.5rem 0;
+`
+
+export const InputForm = styled.input`
+  border: none;
+  border-radius: 50px;
+  width: 18rem;
+  height: 1.8rem;
+  margin: 0.5rem 0;
+`
+
+export const Button = styled.button`
+  border: none;
+  border-radius: 50px;
+  width: 9rem;
+  height: 1.8rem;
+  margin: 0.5rem 0;
+  background-color: mediumslateblue;
 `
--- marzo/react/case-investimento/src/pages/Simulacao/index.js
+++ marzo/react/case-investimento/src/pages/Simulacao/index.js
@@ -7,7 +7,10 @@ import { SimulacaoContainer } from './styles';
 export const Simulacao = () => {
   return (
     <SimulacaoContainer>
-      Simulacao
+      <div>
+        <p>Olá [nome], juntando R$[mensalidade] todo mês, você terá R$[resultado da request] em [tempo].</p>
+        <button>Simular novamente</button>
+      </div>
     </SimulacaoContainer>
   )
 }
